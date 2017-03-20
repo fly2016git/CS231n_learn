26bb6df3e26b420b6be18b85d0210308cc1682d8
zpf_fly2016 <fly2015_zhang@163.com>
2017-03-20 17:33:31 +0800

    KNN算法实现

--- assignment1/classifiers/k_nearest_neighbor.py
+++ assignment1/classifiers/k_nearest_neighbor.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+#http://blog.csdn.net/geekmanong/article/details/51524402
+#http://www.cnblogs.com/daihengchen/p/5754383.html
+#http://blog.csdn.net/han784851198/article/details/53331104
 class KNearestNeighbor(object):
   """ a kNN classifier with L2 distance """
 
@@ -10,7 +13,7 @@ class KNearestNeighbor(object):
     """
     Train the classifier. For k-nearest neighbors this is just 
     memorizing the training data.
-
+    训练分类器。对于KNN算法，此处只需要存储训练数据即可。
     Inputs:
     - X: A numpy array of shape (num_train, D) containing the training data
       consisting of num_train samples each of dimension D.
@@ -23,13 +26,13 @@ class KNearestNeighbor(object):
   def predict(self, X, k=1, num_loops=0):
     """
     Predict labels for test data using this classifier.
-
+    基于该分类器，预测测试数据的标签分类。
     Inputs:
     - X: A numpy array of shape (num_test, D) containing test data consisting
-         of num_test samples each of dimension D.
+         of num_test samples each of dimension D.测试数据集
     - k: The number of nearest neighbors that vote for the predicted labels.
     - num_loops: Determines which implementation to use to compute distances
-      between training points and testing points.
+      between training points and testing points.选择距离算法的实现方法
 
     Returns:
     - y: A numpy array of shape (num_test,) containing predicted labels for the
@@ -50,7 +53,7 @@ class KNearestNeighbor(object):
     """
     Compute the distance between each test point in X and each training point
     in self.X_train using a nested loop over both the training data and the 
-    test data.
+    test data.    两层循环计算L2距离
 
     Inputs:
     - X: A numpy array of shape (num_test, D) containing test data.
@@ -63,15 +66,19 @@ class KNearestNeighbor(object):
     num_test = X.shape[0]
     num_train = self.X_train.shape[0]
     dists = np.zeros((num_test, num_train))
-    for i in xrange(num_test):
-      for j in xrange(num_train):
+    for i in range(num_test):
+      for j in range(num_train):
         #####################################################################
         # TODO:                                                             #
         # Compute the l2 distance between the ith test point and the jth    #
         # training point, and store the result in dists[i, j]. You should   #
         # not use a loop over dimension.                                    #
         #####################################################################
-        pass
+        test_row = X[i, :]
+        train_row = self.X_train[j, :]
+        dists[i, j] = np.sqrt(np.sum((test_row - train_row)**2))
+    
+    return dists
         #####################################################################
         #                       END OF YOUR CODE                            #
         #####################################################################
@@ -80,20 +87,21 @@ class KNearestNeighbor(object):
   def compute_distances_one_loop(self, X):
     """
     Compute the distance between each test point in X and each training point
-    in self.X_train using a single loop over the test data.
+    in self.X_train using a single loop over the test data.   一层循环计算L2距离
 
     Input / Output: Same as compute_distances_two_loops
     """
     num_test = X.shape[0]
     num_train = self.X_train.shape[0]
     dists = np.zeros((num_test, num_train))
-    for i in xrange(num_test):
+    for i in range(num_test):
       #######################################################################
       # TODO:                                                               #
       # Compute the l2 distance between the ith test point and all training #
       # points, and store the result in dists[i, :].                        #
       #######################################################################
-      pass
+      test_row = X[i, :]
+      dists[i,:] = np.sqrt(np.sum(test_row - self.X_train)**2) #numpy广播机制
       #######################################################################
       #                         END OF YOUR CODE                            #
       #######################################################################
@@ -102,7 +110,7 @@ class KNearestNeighbor(object):
   def compute_distances_no_loops(self, X):
     """
     Compute the distance between each test point in X and each training point
-    in self.X_train using no explicit loops.
+    in self.X_train using no explicit loops.    无循环计算L2距离
 
     Input / Output: Same as compute_distances_two_loops
     """
@@ -121,7 +129,7 @@ class KNearestNeighbor(object):
     # HINT: Try to formulate the l2 distance using matrix multiplication    #
     #       and two broadcast sums.                                         #
     #########################################################################
-    pass
+    
     #########################################################################
     #                         END OF YOUR CODE                              #
     #########################################################################
@@ -142,7 +150,7 @@ class KNearestNeighbor(object):
     """
     num_test = dists.shape[0]
     y_pred = np.zeros(num_test)
-    for i in xrange(num_test):
+    for i in range(num_test):
       # A list of length k storing the labels of the k nearest neighbors to
       # the ith test point.
       closest_y = []
